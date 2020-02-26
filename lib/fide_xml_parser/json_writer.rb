646c01b7e6356d477110fcb1d434eb08830435ae
Keith Bennett <keithrbennett@gmail.com>
2020-02-26 23:03:42 +0800

    Supports single and multiple file invocations in the public write method.

--- lib/fide_xml_parser/json_writer.rb
+++ lib/fide_xml_parser/json_writer.rb
@@ -4,7 +4,10 @@ module FideXmlParser
 
   module JsonWriter
 
+    # Checks all input filespecs before processing the first one.
+    # Verifies not nil, ends in ".xml" (case insensitive), and exists as a file.
     def self.validate_input_filespecs(filespecs)
+      filespecs = Array(filespecs)
       bad_filespecs = filespecs.select do |filespec|
         filespec.nil? || (! /\.xml$/.match(filespec)) || (! File.file?(filespec))
       end
@@ -13,23 +16,37 @@ module FideXmlParser
       end
     end
 
-    def self.process_one(input_filespec, json_mode = :pretty)
+
+    # Public entry point to write JSON file(s) from XML.
+    # To write a single file, pass the filespec as the `input_filespecs` parameter.
+    # To write multiple files, pass an array of filespecs as the `input_filespecs` parameter
+    # json_mode: :pretty for human readable JSON, :compact for compact JSON
+    def self.write(input_filespecs, json_mode = :pretty)
+      input_filespecs = Array(input_filespecs)
+      validate_input_filespecs(input_filespecs)
+      input_filespecs.each { |filespec| write_private(filespec, json_mode) }
+    end
+
+
+    # Public entry point to write multiple files.
+    # json_mode: :pretty for human readable JSON, :compact for compact JSON
+    def self.write_multiple(input_filespecs, json_mode = :pretty)
+      validate_input_filespecs(input_filespecs)
+      input_filespecs.each do |input_filespec|
+        write_private(input_filespec, json_mode)
+      end
+    end
+
+
+    # Implementation for writing a single file.
+    # Separated from the public `write` method in order to validate filespecs only once.
+    private
+    def self.write_private(input_filespec, json_mode = :pretty)
       records = FideXmlParser::Processor.parse(File.new(input_filespec))
       json_text = (json_mode == :pretty) ? JSON.pretty_generate(records) : records.to_json
       json_filespec = input_filespec.sub(/\.xml$/, '.json')
       File.write(json_filespec, json_text)
       puts "#{records.size} records processed, #{input_filespec} --> #{json_filespec}"
-    end
-
-
-    # Writes JSON files from the specified XML files
-    # json_mode: :pretty for human readable JSON, :compact for compact JSON
-    def self.write(input_filespecs, json_mode = :pretty)
-      input_filespecs = Array(input_filespecs)
-      validate_input_filespecs(input_filespecs)
-      input_filespecs.each do |input_filespec|
-        process_one(input_filespec)
-      end
     end
   end
 end
